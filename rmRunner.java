a149e48cc072716c5a97e5883bb55baf77184dfc
acJoaog <124536687+acJoaog@users.noreply.github.com>
2023-12-03 14:38:47 -0300

    Delete rmRunner.java

--- rmRunner.java
+++
@@ -1,9 +0,0 @@
-import com.intuit.karate.junit5.Karate;
-
-public class rmRunner {
-    @Karate.Test
-    Karate testStarWars() {
-        return Karate.run("rickMorty").relativeTo(getClass());
-    }    
-
-}
